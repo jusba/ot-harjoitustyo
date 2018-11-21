a1a965b0eda544f18ebe5b35178230086f6acc0b
jusba <jviljami@gmail.com>
2018-11-21 20:30:50 +0200

    Adding random grid creation, not finished

--- Laivanupotus/src/main/java/com/gisyritys/laivanupotus/Grid.java
+++ Laivanupotus/src/main/java/com/gisyritys/laivanupotus/Grid.java
@@ -38,6 +38,15 @@ public class Grid {
             
         }
     }
+    public Location[][] randomGrid(){
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[i].length; j++) {
+                grid[i][j] = new Location(); 
+                
+            }
+        
+        }
+    }
     public int getXSize(){
         return this.xsize;
     }
